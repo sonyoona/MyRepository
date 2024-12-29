b5f3fc73f900c385cf41e40279ee11c32e0deccd
sonyoona <sonyoo0526@naver.com>
2024-12-29 15:30:19 +0900

    First commit in dev C

--- MyProjet/src/myProject/A.java
+++ MyProjet/src/myProject/A.java
@@ -2,7 +2,8 @@ package myProject;
 
 public class A {
 	public static void main(String[] args) {
-		System.out.println("A Project");
+		System.out.println("A Project change2");
+		System.out.println("dev C");
 		
 		B name = new B();
 		name.ProjectName();
