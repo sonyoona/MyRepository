0aabb1949aedc01af90184c2c00637dbf46a74d5
sonyoona <sonyoo0526@naver.com>
2024-12-29 15:31:35 +0900

    Second commit in master

--- MyProjet/src/myProject/A.java
+++ MyProjet/src/myProject/A.java
@@ -3,6 +3,7 @@ package myProject;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("A Project");
+		System.out.println("A has been changed in master");
 		
 		B name = new B();
 		name.ProjectName();
